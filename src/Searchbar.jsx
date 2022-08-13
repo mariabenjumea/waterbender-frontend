7d32e060a33a06ec0f36fe89d41c8f1bef498184
Maria Paula <maria_paula.benjumea_canon@smail.th-koeln.de>
2022-08-13 23:54:22 +0200

    add searchbar object

--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -1,8 +1,26 @@
 import React from "react"
+import "./Searchbar.css"
+
+const SearchbarItem = ({ children }) => {
+    return (
+        <div>
+            <button>
+                {children}
+            </button>
+        </div>
+    )
+}
 
 const Searchbar = () => {
     return (
-        <div>Searchbar</div>
+        <div className="Searchbar" style={{ display: "flex", justifyContent: "space-around" }}>
+            <SearchbarItem>
+                Main Page
+            </SearchbarItem>
+            <SearchbarItem>
+                Add New Plant
+            </SearchbarItem>
+        </div>
     )
 }
 
