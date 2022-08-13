5fbae1f1706d5366a052a298b98d5520575a5d8c
Maria Paula <maria_paula.benjumea_canon@smail.th-koeln.de>
2022-08-13 19:12:02 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Searchbar from "./Searchbar"
+
+const App = () => {
+
+
+    return (
+        <div>
+            <Searchbar />
+            {/* 
+            <Content /> */}
+        </div>
+    )
+}
+
+export default App
