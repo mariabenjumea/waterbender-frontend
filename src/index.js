244a8428009591208e06f637a658aa3a3a88ebf0
Maria Paula <maria_paula.benjumea_canon@smail.th-koeln.de>
2022-08-14 13:02:10 +0200

    add carousel to cards

--- src/index.js
+++ src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import CustomCard from "./CustomCard";
 import Searchbar from "./Searchbar";
+import { MantineProvider } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
 
 const root = ReactDOM.createRoot(document.getElementById('reactroot'));
 
+// fetch('url', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(data),
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log('Success:', data);
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
+
 root.render(
-  <div>
-    <Searchbar />
-    <CustomCard name="Cactus" />
-    <CustomCard name="Blume" />
-  </div>
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <div>
+      <Searchbar />
+      <Carousel slideSize="70%" height={200} slideGap="md" loop withIndicators>
+        <CustomCard name="Cactus" />
+        <CustomCard name="Blume" />
+        <CustomCard name="Sukkulente" />
+      </Carousel>
+    </div>
+  </MantineProvider>
 );
